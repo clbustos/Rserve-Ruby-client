f9dcdd36b7f4054bb2db756859523f821ad54375
Claudio Bustos <clbustos@gmail.com>
2010-06-08 23:21:35 -0400

    Tested on Windows. Benchmark works without RsRuby on Windows

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -5,12 +5,16 @@ $:.unshift(File.dirname(__FILE__)+"/../lib")
 require 'rubygems'
 require 'rserve'
 require 'rinruby'
+begin 
 require 'rsruby'
+rs=RSRuby.instance
+rescue LoadError
+rs=nil
+end
 require 'benchmark'
 data_size=30000
 tries=50
 con=Rserve::Connection.new
-rs=RSRuby.instance
 R.eval("1",false)
 data=data_size.times.map {rand()}
 data_integer=data_size.times.map {rand(100)}
@@ -23,11 +27,11 @@ Benchmark.bm(30) do |x|
   x.report("assign '1' rserve") {
     con.assign("a", 1)
   }
-  
+  if rs
   x.report("assign '1' rsruby") {
     rs.assign('a',1)
   }
-  
+  end
   x.report("assign double(#{data_size}) rinruby") {
     tries.times {
       R.a=data
@@ -36,10 +40,11 @@ Benchmark.bm(30) do |x|
   x.report("assign double(#{data_size}) rserve") {
     con.assign("a", data)
   }
+  if rs
   x.report("assign double(#{data_size}) rsruby") {
     rs.assign("a", data)
   }
-
+end
   x.report("void_eval rinruby") {
     tries.times {
       R.eval("1",false)
@@ -50,15 +55,19 @@ Benchmark.bm(30) do |x|
       con.void_eval("1")
     }
   }
+  if rs
   x.report("void_eval rsruby") {
     tries.times {
       rs.eval("1")
     }
   }
+  end
   # Assign data
   R.a=1
   con.assign('a',1)
+  if rs
   rs.assign('a',1)
+  end
   x.report("get '1' rinruby") {
     tries.times {
       R.pull('a')
@@ -69,16 +78,19 @@ Benchmark.bm(30) do |x|
       con.eval('a').to_ruby
     }
   }
+  if rs
   x.report("get '1' rsruby") {
     tries.times {
       rs.a
     }
   }
-
+end
   
   R.a=data
   con.assign('a',data)
+  if rs
   rs.assign('a',data)
+  end
   x.report("get double(#{data_size}) rinruby") {
     tries.times {
       R.pull('a')
@@ -89,10 +101,13 @@ Benchmark.bm(30) do |x|
       con.eval('a').to_ruby
     }
   }
+  
+  if rs
   x.report("get double(#{data_size}) rsruby") {
     tries.times {
       rs.a
     }
   }
+  end
 
 end
