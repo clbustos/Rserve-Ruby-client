0041bd2e6ca683c5cbda962ba28c6fb51f916bd4
Marco Sandrini <nessche@gmail.com>
2012-11-02 01:03:28 +0200

    Fixed failing specs, split new functionality between With2DNames (applied to Array and Matrix) and With2DSizes (only applied to Array)

--- spec/rserve_with2dnames_spec.rb
+++ spec/rserve_with2dnames_spec.rb
@@ -1,8 +1,4 @@
 require File.expand_path(File.dirname(__FILE__)+"/spec_helper.rb")
-require '../lib/rserve/rexp/string'
-require '../lib/rserve/rexp/integer'
-require '../lib/rserve/rexp/list'
-
 
 describe Rserve::With2DNames do
 
@@ -15,47 +11,27 @@ describe Rserve::With2DNames do
     # 1  2  3  4
     # 5  6  7  8
     # 9  10 11 12
+    @array.extend Rserve::With2DSizes
+    @array.sizes = [3,4]
     @array.extend Rserve::With2DNames
+    @matrix = Matrix[[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    @matrix.extend Rserve::With2DNames
   end
 
-  describe "sizes" do
+  describe "names=" do
 
     context "when passed the correct values" do
 
-      it "should set the size values for rows and columns" do
-        @array.sizes = [3,4]
-        @array.row_size.should == 3
-        @array.column_size.should == 4
-      end
-
-    end
-
-    context "when passed the wrong values" do
-
-      it "should throw if the object passed does not have two elements" do
-        expect{@array.sizes = [5]}.to raise_error ArgumentError
-      end
-
-      it "should throw if the sizes passed do not match the array size" do
-        expect{@array.sizes = [1,5]}.to raise_error ArgumentError
-      end
-
-    end
-
-  end
-
-  describe "names" do
-
-    context "when passed the correct values" do
-
-      before do
-        @array.sizes = [3,4]
-      end
-
-      it "should set the names for rows and columns" do
+      it "should set the names for rows and columns for an array" do
         @array.names = [%w(r1 r2 r3),%w(c1 c2 c3 c4)]
         @array.row_names.should == %w(r1 r2 r3)
         @array.column_names.should == %w(c1 c2 c3 c4)
+      end
+
+      it "should set the names for rows and columns for an array" do
+        @matrix.names = [%w(r1 r2 r3),%w(c1 c2 c3 c4)]
+        @matrix.row_names.should == %w(r1 r2 r3)
+        @matrix.column_names.should == %w(c1 c2 c3 c4)
       end
 
     end
@@ -64,38 +40,79 @@ describe Rserve::With2DNames do
 
       it "should throw if the object passed does not have two elements" do
         expect{@array.names = [%w(r1 r2 r3)]}.to raise_error ArgumentError
+        expect{@matrix.names = [%w(r1 r2 r3)]}.to raise_error ArgumentError
       end
 
       it "should throw if the sizes passed do not match the array size" do
         expect{@array.names = [%w(r1 r2 r3 r4),%w(c1 c2 c3 c4)]}.to raise_error ArgumentError
+        expect{@matrix.names = [%w(r1 r2 r3 r4),%w(c1 c2 c3 c4)]}.to raise_error ArgumentError
       end
 
     end
 
   end
 
-  describe "two_d_at" do
+  describe "row_by_name" do
 
     before do
-      @array.sizes = [3,4]
       @array.names = [%w(r1 r2 r3),%w(c1 c2 c3 c4)]
+      @matrix.names = [%w(r1 r2 r3),%w(c1 c2 c3 c4)]
     end
 
+    context "when the name is present" do
 
-    it "should return the correct values using integer indices" do
-      @array.two_d_at(0,0).should == 1
-      @array.two_d_at(2,0).should == 9
-      @array.two_d_at(0,3).should == 4
-      @array.two_d_at(2,3).should == 12
+      it "should return the correct row for arrays" do
+        @array.row_by_name("r2").should == [5, 6, 7, 8]
+      end
+
+      it "should return the correct row for matrices" do
+        @matrix.row_by_name("r2").to_a.should == [5, 6, 7, 8]
+      end
+
     end
 
-    it "should return the correct values using names" do
-      @array.two_d_at("r1","c2").should == 2
-      @array.two_d_at("r2","c1").should == 5
+    context "when the index is out of range" do
+
+      it "should return nil" do
+        @array.row_by_name("not_there").should be_nil
+        @matrix.row_by_name("not_there").should be_nil
+      end
+
     end
 
+  end
 
- end
+  describe "column_by_name" do
+
+    before do
+      @array.names = [%w(r1 r2 r3),%w(c1 c2 c3 c4)]
+      @matrix.names = [%w(r1 r2 r3),%w(c1 c2 c3 c4)]
+    end
+
+    context "when the name is present" do
+
+      it "should return the correct column for arrays" do
+        @array.column_by_name("c1").should == [1, 5, 9]
+      end
+
+      it "should return the correct column for matrix" do
+        @matrix.column_by_name("c1").to_a.should == [1, 5, 9]
+      end
+
+    end
+
+    context "when the name is not present" do
+
+      it "should return nil" do
+        @array.column_by_name("not_there").should be_nil
+        @matrix.column_by_name("not_there").should be_nil
+      end
+
+    end
+
+  end
+
+
 
 
 
